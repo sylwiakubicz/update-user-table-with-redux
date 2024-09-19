d9db2a40cc7da408a7422b46691698fe0bbece74
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-19 21:05:51 +0200

    feat: Add sorting arrows next to filter inputs in UserTableFilterRow.

--- src/components/SortArrows.tsx
+++ src/components/SortArrows.tsx
@@ -1,9 +1,9 @@
 
 function SortArrows() {
     return (
-        <div className='flex flex-col justify-between h-[1.6rem]'>
-            <div className='w-0 h-0 border-x-[0.5rem] border-x-transparent border-b-[0.7rem] border-b-dark-dark-text cursor-pointer'></div>
-            <div className='w-0 h-0 border-x-[0.5rem] border-x-transparent border-t-[0.7rem] border-t-dark-dark-text cursor-pointer'></div>
+        <div className='flex flex-col justify-between h-[1.35rem]'>
+            <div className='w-0 h-0 border-x-[0.45rem] border-x-transparent border-b-[0.6rem] border-b-dark-dark-text cursor-pointer'></div>
+            <div className='w-0 h-0 border-x-[0.45rem] border-x-transparent border-t-[0.6rem] border-t-dark-dark-text cursor-pointer'></div>
         </div>
     )
 }
--- src/components/UserTableFilterRow.tsx
+++ src/components/UserTableFilterRow.tsx
@@ -17,43 +17,56 @@ function UserTableFilterRow() {
     }, [dispatch, usersFilters]);
 
     return (
-    <tr >
+    <tr>
         <th className='pl-6 pb-2'>
-            <input
-            type="text"
-            placeholder="Filter by name"
-            value={usersFilters.name}
-            onChange={(e) => dispatch(setNameFilter(e.target.value))}
-            className="w-full px-2 py-1 h-9 font-light placeholder:opacity-70 border-[1px] border-gray-200 dark:border-dark-secondary dark:bg-dark-text dark:placeholder:text-dark-secondary dark:text-dark-secondary focus-visible:outline-none"
-            />
-        </th>
-        <th className='pl-6 pb-2 flex flex-row items-center gap-3'>
-            <input
-            type="text"
-            placeholder="Filter by username"
-            value={usersFilters.username}
-            onChange={(e) => dispatch(setUsernameFilter(e.target.value))}
-            className="flex-grow px-2 py-1 h-9 font-light placeholder:opacity-70 border-[1px] border-gray-200 dark:border-dark-secondary dark:bg-dark-text dark:placeholder:text-dark-secondary dark:text-dark-secondary focus-visible:outline-none"
-            />
-            <SortArrows />
+            <div className="flex items-center gap-2">
+                <input
+                type="text"
+                placeholder="Filter by name"
+                value={usersFilters.name}
+                onChange={(e) => dispatch(setNameFilter(e.target.value))}
+                className="flex-grow px-2 py-1 h-9 font-light placeholder:opacity-70 border-[1px] border-gray-200 dark:border-dark-secondary dark:bg-dark-text dark:placeholder:text-dark-secondary dark:text-dark-secondary focus-visible:outline-none"
+                />
+                <SortArrows />
+            </div>
+
         </th>
         <th className='pl-6 pb-2'>
-            <input
-            type="text"
-            placeholder="Filter by email"
-            value={usersFilters.email}
-            onChange={(e) => dispatch(setEmailFilter(e.target.value))}
-            className="w-full px-2 py-1 h-9 font-light placeholder:opacity-70 border-[1px] border-gray-200 dark:border-dark-secondary dark:bg-dark-text dark:placeholder:text-dark-secondary dark:text-dark-secondary focus-visible:outline-none"
-            />
+            <div className="flex items-center gap-2">
+                <input
+                type="text"
+                placeholder="Filter by username"
+                value={usersFilters.username}
+                onChange={(e) => dispatch(setUsernameFilter(e.target.value))}
+                className="flex-grow px-2 py-1 h-9 font-light placeholder:opacity-70 border-[1px] border-gray-200 dark:border-dark-secondary dark:bg-dark-text dark:placeholder:text-dark-secondary dark:text-dark-secondary focus-visible:outline-none"
+                />
+                <SortArrows />
+            </div>
         </th>
-        <th className='pl-6 pr-6 pb-2'>
-            <input
-            type="text"
-            placeholder="Filter by phone"
-            value={usersFilters.phone}
-            onChange={(e) => dispatch(setPhoneFilter(e.target.value))}
-            className="w-full px-2 py-1 h-9 font-light placeholder:opacity-70 border-[1px] border-gray-200 dark:border-dark-secondary dark:bg-dark-text dark:placeholder:text-dark-secondary dark:text-dark-secondary focus-visible:outline-none"
-            />
+        <th className='pl-6 pb-2'>
+            <div className="flex items-center gap-2">
+                <input
+                type="text"
+                placeholder="Filter by email"
+                value={usersFilters.email}
+                onChange={(e) => dispatch(setEmailFilter(e.target.value))}
+                className="flex-grow px-2 py-1 h-9 font-light placeholder:opacity-70 border-[1px] border-gray-200 dark:border-dark-secondary dark:bg-dark-text dark:placeholder:text-dark-secondary dark:text-dark-secondary focus-visible:outline-none"
+                />
+                <SortArrows />
+            </div>
+
+        </th>
+        <th className='pl-6 pb-2 pr-6'>
+            <div className="flex items-center gap-2">
+                <input
+                type="text"
+                placeholder="Filter by phone"
+                value={usersFilters.phone}
+                onChange={(e) => dispatch(setPhoneFilter(e.target.value))}
+                className="flex-grow px-2 py-1 h-9 font-light placeholder:opacity-70 border-[1px] border-gray-200 dark:border-dark-secondary dark:bg-dark-text dark:placeholder:text-dark-secondary dark:text-dark-secondary focus-visible:outline-none"
+                />
+                <SortArrows />
+            </div>
         </th>
     </tr>
     )
