e2634cef09ca46417a2a6c35d8f46a647e1e48c1
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-19 20:25:07 +0200

    Feat: Create SortArrows component.

---
+++ src/components/SortArrows.tsx
@@ -0,0 +1,11 @@
+
+function SortArrows() {
+    return (
+        <div className='flex flex-col justify-between h-[1.6rem]'>
+            <div className='w-0 h-0 border-x-[0.5rem] border-x-transparent border-b-[0.7rem] border-b-dark-dark-text cursor-pointer'></div>
+            <div className='w-0 h-0 border-x-[0.5rem] border-x-transparent border-t-[0.7rem] border-t-dark-dark-text cursor-pointer'></div>
+        </div>
+    )
+}
+
+export default SortArrows
