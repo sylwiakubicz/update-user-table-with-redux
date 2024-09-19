1ef6b3923b5c532678d1280be4aa1ff77ecbefc1
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-19 20:36:15 +0200

    feat: Add sorting arrows for testing in 'Name' and 'Username' columns

--- src/components/UserTableFilterRow.tsx
+++ src/components/UserTableFilterRow.tsx
@@ -3,6 +3,7 @@ import {applyFilters} from "../redux/user/userSlice"
 import { setNameFilter, setUsernameFilter, setEmailFilter, setPhoneFilter } from '../redux/usersFilters/userFiltersSlice';
 import { AppDispatch, RootState } from '../redux/store';
 import { useDispatch, useSelector } from 'react-redux';
+import SortArrows from './SortArrows';
 
 
 function UserTableFilterRow() {
@@ -26,14 +27,15 @@ function UserTableFilterRow() {
             className="w-full px-2 py-1 h-9 font-light placeholder:opacity-70 border-[1px] border-gray-200 dark:border-dark-secondary dark:bg-dark-text dark:placeholder:text-dark-secondary dark:text-dark-secondary focus-visible:outline-none"
             />
         </th>
-        <th className='pl-6 pb-2'>
+        <th className='pl-6 pb-2 flex flex-row items-center gap-3'>
             <input
             type="text"
             placeholder="Filter by username"
             value={usersFilters.username}
             onChange={(e) => dispatch(setUsernameFilter(e.target.value))}
-            className="w-full px-2 py-1 h-9 font-light placeholder:opacity-70 border-[1px] border-gray-200 dark:border-dark-secondary dark:bg-dark-text dark:placeholder:text-dark-secondary dark:text-dark-secondary focus-visible:outline-none"
+            className="flex-grow px-2 py-1 h-9 font-light placeholder:opacity-70 border-[1px] border-gray-200 dark:border-dark-secondary dark:bg-dark-text dark:placeholder:text-dark-secondary dark:text-dark-secondary focus-visible:outline-none"
             />
+            <SortArrows />
         </th>
         <th className='pl-6 pb-2'>
             <input
--- src/components/UserTableHead.tsx
+++ src/components/UserTableHead.tsx
@@ -1,11 +1,15 @@
 import UserTableFilterRow from './UserTableFilterRow';
 import ResetButton from './ResetButton';
+import SortArrows from './SortArrows';
 
 function UserTableHead() {
     return (
     <thead  className="text-[1rem] text-gray-700 uppercase bg-custom-light-gray dark:bg-dark-secondary dark:text-dark-text relative">
         <tr>
-            <th scope="col" className="px-6 pt-3 pb-1 text-akcent-color dark:text-dark-akcent-color" >Name</th>
+            <th scope="col" className="px-6 pt-3 pb-1 text-akcent-color dark:text-dark-akcent-color flex gap-3 align-center" >
+                <span className="pt-0.5">Name</span>
+                <SortArrows />
+            </th>
             <th scope="col" className="px-6 pt-3 pb-1 text-akcent-color dark:text-dark-akcent-color">Username</th>
             <th scope="col" className="px-6 pt-3 pb-1 text-akcent-color dark:text-dark-akcent-color">Email</th>
             <th scope="col" className="px-6 pt-3 pb-1 text-akcent-color dark:text-dark-akcent-color">Phone number</th>
