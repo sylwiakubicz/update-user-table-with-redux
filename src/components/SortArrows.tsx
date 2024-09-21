2f81a2d7e21d0466908c7dcbeb56cb66a0185785
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-21 18:06:19 +0200

    feat: integrate Redux state and dispatch into SortArrows component

--- src/components/SortArrows.tsx
+++ src/components/SortArrows.tsx
@@ -1,9 +1,34 @@
+import { AppDispatch, RootState } from '../redux/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { applySort } from '../redux/user/userSlice';
+import { useEffect } from 'react';
+import { sortAsc, sortDesc } from '../redux/usersFilters/userSortSlice';
 
-function SortArrows() {
+interface SortArrowsProps {
+    colName : string
+}
+
+function SortArrows({colName} : SortArrowsProps) {
+
+
+    const sortProperties = useSelector((state : RootState) => state.userSort)
+    const dispatch = useDispatch<AppDispatch>();
+
+    useEffect(() => {
+        dispatch(applySort(sortProperties))
+        console.log(sortProperties)
+    }, [dispatch, sortProperties])
+
     return (
         <div className='flex flex-col justify-between h-[1.35rem]'>
-            <div className='w-0 h-0 border-x-[0.45rem] border-x-transparent border-b-[0.6rem] border-b-dark-dark-text cursor-pointer'></div>
-            <div className='w-0 h-0 border-x-[0.45rem] border-x-transparent border-t-[0.6rem] border-t-dark-dark-text cursor-pointer'></div>
+            <div 
+                className='w-0 h-0 border-x-[0.45rem] border-x-transparent border-b-[0.6rem] border-b-dark-dark-text cursor-pointer'
+                onClick={() => dispatch(sortAsc(colName))}
+                ></div>
+            <div 
+                className='w-0 h-0 border-x-[0.45rem] border-x-transparent border-t-[0.6rem] border-t-dark-dark-text cursor-pointer'
+                onClick={() => dispatch(sortDesc(colName))}
+                ></div>
         </div>
     )
 }
