d911063698f0888e59a4958c7c35533ed738faa7
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-21 17:58:53 +0200

    fix: resolve key error by ensuring unique keys for UserTableDataRow

--- src/components/UserTableDataRow.tsx
+++ src/components/UserTableDataRow.tsx
@@ -2,7 +2,7 @@ import { User } from "../interfaces/User";
 
 function UserTableDataRow({ user }: { user: User }) {
     return (
-        <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-custom-light-gray even:dark:bg-dark-secondary border-b dark:border-dark-text">
+        <tr key={user.id} className="odd:bg-white odd:dark:bg-gray-900 even:bg-custom-light-gray even:dark:bg-dark-secondary border-b dark:border-dark-text">
             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                 {user.name}
             </th>
